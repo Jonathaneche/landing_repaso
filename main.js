21289c1dfc2bb3eb74561974523fd97ce33874e9
Jonathaneche <jonathan.eche@gmail.com>
2023-01-17 08:41:03 +0100

    agregar las media queries y la funcion

---
+++ main.js
@@ -0,0 +1,17 @@
+
+    let x = document.getElementById("inicial").value;
+
+    let y = document.getElementById("final").value;
+
+function calcularRango(){
+
+
+
+    for (x = document.getElementById("inicial").value, y = document.getElementById("final").value; x <= y ; x++){
+     
+        //document.getElementById("rango").write(x);
+        console.log(x);
+    }
+
+    
+}
